4ddd7b31091e5d1df7ab1b8af4917da63b012937
Matvey24 <perelman.matvey@gmail.com>
2024-08-27 00:07:49 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,15 @@
+# RimWorld-FPSStabilizer
+
+RimWorld mod that overrides "worst allowed fps" from 22 fps to unity target frame rate.
+
+This softly reduces amount of ticks per frame to stabilize FPS in cost of slightly lower TPS
+
+## License
+Assets are [CC-BY-NC-SA](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+Code is derivate from Ludeon Studio
+
+
+[![Version](https://img.shields.io/badge/Rimworld-1.4-green.svg)](http://rimworldgame.com/)
+[![Version](https://img.shields.io/badge/Rimworld-1.5-green.svg)](http://rimworldgame.com/)
+[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-blue.svg)](http://creativecommons.org/licenses/by-nc-sa/4.0/)
---
+++ Source/FPSStabilizer/Main.cs
@@ -0,0 +1,53 @@
+﻿using Verse;
+namespace FPSStabilizer
+{
+    using HarmonyLib;
+    using System.Reflection;
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+    using System.Linq;
+    using UnityEngine;
+
+    [StaticConstructorOnStartup]
+    public static class Main
+    {
+        public static float frametime;
+        static Main(){
+            frametime = 1000f / Application.targetFrameRate;
+
+            Harmony harm = new Harmony("matvey24.FPSStabilizer");
+            MethodBase original = AccessTools.Method(typeof(TickManager), nameof(TickManager.TickManagerUpdate));
+            HarmonyMethod transpiler = new HarmonyMethod(typeof(Main), nameof(Transpiler));
+            try
+            {
+                MethodInfo methodInfo = harm.Patch(original, null, null, transpiler, null);
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Error during patching {original} with: transpliter {transpiler?.method}\n{e}");
+            }
+        }
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            bool patched_something = false;
+            var l = instructions.ToList();
+            foreach (var c in l)
+            {
+                if(!patched_something && c.opcode == OpCodes.Ldc_R4 && c.operand is float)
+                {
+                    if ((float)c.operand > 10) {
+                        // this instruction is probably milliseconds
+                        c.operand = frametime;
+                        patched_something = true;
+                        Log.Message("FPSStabilier patched to " + 1000 / frametime + " FPS");
+                    }
+                }
+                yield return c;
+            }
+            if (!patched_something) {
+                Log.Error("FPSStabilier not patched, instruction was not found");
+            }
+        }
+    }
+}
